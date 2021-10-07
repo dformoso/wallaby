b9e5b503e3fb5f214d7feeab239beb104ca05aee
daniel <daniel.martinez.formoso@gmail.com>
2021-10-07 15:14:38 +1100

    added hpv18 fa

--- workflows/scripts/run_multi_donor_recipient.sh
+++ workflows/scripts/run_multi_donor_recipient.sh
@@ -11,6 +11,6 @@ java \
   -Dconfig.file=../../cromwell_config/local_cpu.conf \
   -jar ../../binaries/cromwell-69.jar \
     run ../multi_donor_recipient.wdl \
-    --inputs ../inputs/multi_donor_recipient_hpv16.json \
+    --inputs ../inputs/multi_donor_recipient_hpv18.json \
     --options ../../cromwell_config/options_local.json
     
